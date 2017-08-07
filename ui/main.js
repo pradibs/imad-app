c711d2b43c7ae8354bd8a33e8be8a4c33a331429
pradibs <pradi.bs15@gmail.com>
2017-08-07 16:59:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,12 +22,22 @@ var counter =0;
 button.onclick= function(){
   
   //Make a request to the counter endpoint
+  var request = new XMLHttpResponse();
   
   //capture the response and store it in a variable
+  request.onreadystatechange= function(){
+      if(request.readyState=== XMLHttpRequest.Done){
+          //Take some action
+          if(request.status=== 200){
+              var counter= request.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML= counter.toString();
+          }
+      }
+  };
   
-  //render the variable  in the correct span
-  counter = counter+1;
-  var span = document.getElementById("count");
-  span.innerHTML=counter.toString();
+ //make request
+ request.open('GET','http://pradibs.imad.hasura-app.io/counter',true);
+ request.send(null);
     
 };
