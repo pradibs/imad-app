c3190e662f0e8e2bed0c8a81a3dbb6d38033729b
pradibs <pradi.bs15@gmail.com>
2017-08-07 19:14:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,8 +41,7 @@ button.onclick= function(){
 };
 
 //submit name
-var nameInput= document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 
 submit.onclick= function(){
@@ -71,6 +70,8 @@ submit.onclick= function(){
   };
   
  //make request
+ var nameInput= document.getElementById('name');
+ var name = nameInput.value;
  request.open('GET','http://pradibs.imad.hasura-app.io/submit-name?name='+ name,true);
  request.send(null);
     
