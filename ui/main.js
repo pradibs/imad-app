d2305d9dde4eef1cf2127a04cf362b612143e9ce
pradibs <pradi.bs15@gmail.com>
2017-08-07 18:19:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,3 +39,22 @@ button.onclick= function(){
  request.open('GET','http://pradibs.imad.hasura-app.io/counter',true);
  request.send(null);
 };
+
+//submit name
+var nameInput= document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+submit.onclick= function(){
+    //make request to the server and send name
+    //capture list of  names  and render it as a list
+    var names= ['name1', 'name2', 'name3']
+    var list='';
+    for (i=0; i<names.length; i++)
+    {
+     list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('nameList');
+    ul.innerHTML=list;
+};
