41009cb561acf380df0d442630663d646b18d3b4
pradibs <pradi.bs15@gmail.com>
2017-08-07 15:53:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('Loaded!');
 
 var img=document.getElementById('madi');
@@ -11,4 +12,22 @@ function moveright(){
 img.onclick= function(){
     var interval = setInterval(moveright,50);
     //img.style.marginLeft='100px';
+};
+*/
+
+//Counter
+var button = document.getElementById('counter');
+
+var counter =0;
+button.onClick= function(){
+  
+  //Make a request to the counter endpoint
+  
+  //capture the response and store it in a variable
+  
+  //render the variable  in the correct span
+  counter = counter+1;
+  var span = document.getElementById("count");
+  span.innerHTML=counter.toString();
+    
 };
