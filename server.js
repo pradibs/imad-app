71be83518d58dd837c681b16f8f5b6222ef251e3
pradibs <pradi.bs15@gmail.com>
2017-08-23 12:45:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/login',function(req, res){
                if(hashedPassword===dbString ){
                    //set the session
                    //console.log('test');
-                   req.session.auth = {userId: result.rows[0].id};
+                   //req.session.auth = {userId: result.rows[0].id};
                    //set cookie with a session id
                    //internally, on the server side, it maps the session id to an object
                    //{auth: {userId}}
